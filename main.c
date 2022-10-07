b51695c3caa936ce5c617616fc9500066009233d
jared0192galicia <jared0192galicia@gmail.com>
2022-10-06 23:52:28 -0500

    Use signals in button's and funtion for signals

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget, gpointer   data){
-  g_print ("Hello World\n");
+// Funtion execute for signal the widgets generate. Arg need
+static void print_hello (GtkWidget *widget, gpointer   data){
+  int num = 10;
+  int num2 = 11;
+  g_print ("Hello World %d\n", num + num2);
 }
 
 static void activate (GtkApplication *app, gpointer user_data) {
@@ -17,6 +19,7 @@ static void activate (GtkApplication *app, gpointer user_data) {
 
   // Set properties for winow 
   window = gtk_application_window_new (app);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
   gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
@@ -27,8 +30,9 @@ static void activate (GtkApplication *app, gpointer user_data) {
   // Add button_box to fixed en position absolute
   gtk_fixed_put(GTK_FIXED(fixed), button_box, 170, 150);
   button = gtk_button_new_with_label ("Hello World");
-  // g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  // g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  // Send señal (Event) clicked, execute funtion arg G_CALLBACK
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  // g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window); press button = Close window
   gtk_container_add (GTK_CONTAINER (button_box), button);
 
   // Set properties for label
