0116bc34694fe5dd5985c328e135d401539f4315
jared0192galicia <jared0192galicia@gmail.com>
2022-10-06 23:03:22 -0500

    Label's and position on flixedgit add .

--- main.c
+++ main.c
@@ -6,22 +6,35 @@ print_hello (GtkWidget *widget, gpointer   data){
 }
 
 static void activate (GtkApplication *app, gpointer user_data) {
+  GtkWidget *fixed;
   GtkWidget *window;
   GtkWidget *button;
   GtkWidget *button_box;
+  GtkWidget *label;
 
+  // Create container fixed
+  fixed = gtk_fixed_new();
+
+  // Set properties for winow 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
+  gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
+  // Set properties for button and button-box
   button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
 
+  // Add button_box to fixed en position absolute
+  gtk_fixed_put(GTK_FIXED(fixed), button_box, 170, 150);
   button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  // g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  // g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
   gtk_container_add (GTK_CONTAINER (button_box), button);
 
+  // Set properties for label
+  label = gtk_label_new("soy un label");
+  gtk_fixed_put(GTK_FIXED(fixed), label, 170, 20);
+  gtk_container_add(GTK_CONTAINER(window), fixed);
   gtk_widget_show_all (window);
 }
 
@@ -36,3 +49,5 @@ int main (int argc, char **argv) {
 
   return status;
 }
+
+// compiler: gcc `pkg-config --cflags gtk+-3.0` -o auto main.c `pkg-config --libs gtk+-3.0`
